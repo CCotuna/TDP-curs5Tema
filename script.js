5e9cb346e859970679fff71231c59dccc10aa8f5
CCotuna <calin.cotuna@emanuel.ro>
2023-04-04 22:29:57 +0300

    Added the function sun() - drawing the sun

--- script.js
+++ script.js
@@ -3,5 +3,19 @@ function setup(){
 }
 
 function draw(){
-    background('rgb(227,134,68)');
+    sun();
+}
+
+
+function sun(){
+   circle(650,325, 80)
+   triangle(650, 275, 675, 285, 675, 250)  
+   triangle(690, 295, 700, 315, 725, 290)
+   triangle(700, 330, 695, 350, 730, 350)
+   triangle(685, 364, 665, 380, 695, 400)
+   triangle(645, 375, 620, 365, 625, 405)
+   triangle(605, 350, 600, 320, 570, 340)
+   triangle(605, 295, 625, 280, 600, 260)   
+
+   fill('yellow')
 }
