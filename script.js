14af476df110089ae979a40161ea043fb97fd8cb
CCotuna <calin.cotuna@emanuel.ro>
2023-04-04 23:00:07 +0300

    ADD function drawPlanets

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function draw(){
     
     drawEllipse();
     drawSun();
+    drawPlanets();
 }
 
 
 function drawSun(){
+   fill('yellow')
    circle(650,325, 80)
    triangle(650, 275, 675, 285, 675, 250)  
    triangle(690, 295, 700, 315, 725, 290)
@@ -18,15 +20,29 @@ function drawSun(){
    triangle(645, 375, 620, 365, 625, 405)
    triangle(605, 350, 600, 320, 570, 340)
    triangle(605, 295, 625, 280, 600, 260)   
-
-//    fill('yellow')
+   
+   
 }
 
 function drawEllipse(){
     ellipse(650, 325, 1200, 450)
     ellipse(620, 325, 1000, 420)
-    ellipse(610, 325, 860, 380)
+    ellipse(620, 325, 860, 380)
     ellipse(635, 325, 700, 340)
     ellipse(650, 315, 550, 310)
     
-   }
+}
+
+function drawPlanets(){
+    fill('rgb(99, 228, 242)')
+    circle(480,193, 15)
+    fill('rgb(214, 159, 190)');
+    circle(340, 235, 25)
+    fill('purple')
+    circle(260, 225, 30)
+    fill('rgb(168, 168, 168)')
+    circle(1000, 190, 40)
+    fill('rgb(95, 194, 111)')
+    circle(1180, 420, 80)
+    fill('white');
+}
